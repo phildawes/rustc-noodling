32eeca95603c2c6a50dac721450745b9fde385c5
Phil Dawes <phil@phildawes.net>
2015-04-21 20:07:27 +0100

    Add mytypeck

---
+++ mytypeck/src/diagnostics.rs
@@ -0,0 +1,187 @@
+// Copyright 2014 The Rust Project Developers. See the COPYRIGHT
+// file at the top-level directory of this distribution and at
+// http://rust-lang.org/COPYRIGHT.
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+#![allow(non_snake_case)]
+
+register_diagnostics! {
+    E0023,
+    E0024,
+    E0025,
+    E0026,
+    E0027,
+    E0029,
+    E0030,
+    E0031,
+    E0033,
+    E0034,
+    E0035,
+    E0036,
+    E0038,
+    E0040, // explicit use of destructor method
+    E0044,
+    E0045,
+    E0046,
+    E0049,
+    E0050,
+    E0053,
+    E0054,
+    E0055,
+    E0057,
+    E0059,
+    E0060,
+    E0061,
+    E0062,
+    E0063,
+    E0066,
+    E0067,
+    E0068,
+    E0069,
+    E0070,
+    E0071,
+    E0072,
+    E0073,
+    E0074,
+    E0075,
+    E0076,
+    E0077,
+    E0081,
+    E0082,
+    E0083,
+    E0084,
+    E0085,
+    E0086,
+    E0087,
+    E0088,
+    E0089,
+    E0090,
+    E0091,
+    E0092,
+    E0093,
+    E0094,
+    E0101,
+    E0102,
+    E0103,
+    E0104,
+    E0106,
+    E0107,
+    E0116,
+    E0117,
+    E0118,
+    E0119,
+    E0120,
+    E0121,
+    E0122,
+    E0123,
+    E0124,
+    E0127,
+    E0128,
+    E0129,
+    E0130,
+    E0131,
+    E0132,
+    E0141,
+    E0159,
+    E0163,
+    E0164,
+    E0166,
+    E0167,
+    E0168,
+    E0172,
+    E0173, // manual implementations of unboxed closure traits are experimental
+    E0174, // explicit use of unboxed closure methods are experimental
+    E0178,
+    E0182,
+    E0183,
+    E0184,
+    E0185,
+    E0186,
+    E0187, // can't infer the kind of the closure
+    E0188, // types differ in mutability
+    E0189, // can only cast a boxed pointer to a boxed object
+    E0190, // can only cast a &-pointer to an &-object
+    E0191, // value of the associated type must be specified
+    E0192, // negative imples are allowed just for `Send` and `Sync`
+    E0193, // cannot bound type where clause bounds may only be attached to types
+           // involving type parameters
+    E0194,
+    E0195, // lifetime parameters or bounds on method do not match the trait declaration
+    E0196, // cannot determine a type for this closure
+    E0197, // inherent impls cannot be declared as unsafe
+    E0198, // negative implementations are not unsafe
+    E0199, // implementing trait is not unsafe
+    E0200, // trait requires an `unsafe impl` declaration
+    E0201, // duplicate method in trait impl
+    E0202, // associated items are not allowed in inherent impls
+    E0203, // type parameter has more than one relaxed default bound,
+           // and only one is supported
+    E0204, // trait `Copy` may not be implemented for this type; field
+           // does not implement `Copy`
+    E0205, // trait `Copy` may not be implemented for this type; variant
+           // does not implement `copy`
+    E0206, // trait `Copy` may not be implemented for this type; type is
+           // not a structure or enumeration
+    E0207, // type parameter is not constrained by the impl trait, self type, or predicate
+    E0208,
+    E0209, // builtin traits can only be implemented on structs or enums
+    E0210, // type parameter is not constrained by any local type
+    E0211,
+    E0212, // cannot extract an associated type from a higher-ranked trait bound
+    E0213, // associated types are not accepted in this context
+    E0214, // parenthesized parameters may only be used with a trait
+    E0215, // angle-bracket notation is not stable with `Fn`
+    E0216, // parenthetical notation is only stable with `Fn`
+    E0217, // ambiguous associated type, defined in multiple supertraits
+    E0218, // no associated type defined
+    E0219, // associated type defined in higher-ranked supertrait
+    E0220, // associated type not found for type parameter
+    E0221, // ambiguous associated type in bounds
+    E0222, // variadic function must have C calling convention
+    E0223, // ambiguous associated type
+    E0224, // at least one non-builtin train is required for an object type
+    E0225, // only the builtin traits can be used as closure or object bounds
+    E0226, // only a single explicit lifetime bound is permitted
+    E0227, // ambiguous lifetime bound, explicit lifetime bound required
+    E0228, // explicit lifetime bound required
+    E0229, // associated type bindings are not allowed here
+    E0230, // there is no type parameter on trait
+    E0231, // only named substitution parameters are allowed
+    E0232, // this attribute must have a value
+    E0233,
+    E0234, // `for` loop expression has type which does not implement the `Iterator` trait
+    E0235, // structure constructor specifies a structure of type but
+    E0236, // no lang item for range syntax
+    E0237, // no lang item for range syntax
+    E0238, // parenthesized parameters may only be used with a trait
+    E0239, // `next` method of `Iterator` trait has unexpected type
+    E0240,
+    E0241,
+    E0242, // internal error looking up a definition
+    E0243, // wrong number of type arguments
+    E0244, // wrong number of type arguments
+    E0245, // not a trait
+    E0246, // illegal recursive type
+    E0247, // found module name used as a type
+    E0248, // found value name used as a type
+    E0249, // expected constant expr for array length
+    E0250, // expected constant expr for array length
+    E0318, // can't create default impls for traits outside their crates
+    E0319, // trait impls for defaulted traits allowed just for structs/enums
+    E0320, // recursive overflow during dropck
+    E0321, // extended coherence rules for defaulted traits violated
+    E0322, // cannot implement Sized explicitly
+    E0366, // dropck forbid specialization to concrete type or region
+    E0367, // dropck forbid specialization to predicate not in struct/enum
+    E0368, // binary operation `<op>=` cannot be applied to types
+    E0369, // binary operation `<op>` cannot be applied to types
+    E0371, // impl Trait for Trait is illegal
+    E0372  // impl Trait for Trait where Trait is not object safe
+}
+
+__build_diagnostic_array! { DIAGNOSTICS }
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@
 
 extern crate rustc;
 extern crate rustc_driver;
-extern crate rustc_typeck;
+extern crate mytypeck; // copy of rustc_typeck with some private fns made public
 extern crate rustc_resolve;
 extern crate syntax;
 extern crate time;
@@ -16,26 +16,18 @@ use syntax::diagnostic::Emitter;
 use rustc_driver::driver;
 // use rustc_lint;
 use rustc_resolve as resolve;
-use rustc_typeck::middle::lang_items;
-use rustc_typeck::middle::region;
-use rustc_typeck::middle::resolve_lifetime;
-use rustc_typeck::middle::stability;
-// use rustc_typeck::middle::subst;
-// use rustc_typeck::middle::subst::Subst;
-use rustc_typeck::middle::ty;
-// use rustc_typeck::middle::ty_relate::TypeRelation;
-// use rustc_typeck::middle::infer;
-// use rustc_typeck::middle::infer::lub::Lub;
-// use rustc_typeck::middle::infer::glb::Glb;
-// use rustc_typeck::middle::infer::sub::Sub;
-// use rustc_typeck::util::ppaux::{ty_to_string, Repr, UserString};
-// use rustc::session::{self,config};
+use mytypeck::middle::lang_items;
+use mytypeck::middle::region;
+use mytypeck::middle::resolve_lifetime;
+use rustc::middle::stability;
+use rustc::middle::ty;
 use syntax::{ast_map};
 use syntax::codemap;
 use syntax::codemap::{Span, CodeMap};
 use syntax::diagnostic::{Level, RenderSpan};
-// use syntax::parse::token;
 use rustc::metadata::creader::CrateReader;
+use rustc::middle::infer::new_infer_ctxt;
+use rustc::util::nodemap::FnvHashMap;
 
 use std::io::Read;
 
@@ -63,26 +55,25 @@ impl Emitter for MyErrorEmitter {
     }
 }
 
+use mytypeck::CrateCtxt;
+use std::cell::RefCell;
+use mytypeck::collect;
+use mytypeck::variance;
+use mytypeck::coherence;
+use mytypeck::check;
+//use mytypeck::check_for_entry_fn;
+
 fn main() {
+
+    let fname = std::env::args().nth(1).unwrap();
 
     let t0 = time::precise_time_s();
 
     let mut options =
         config::basic_options();
 
-    let fpath = Path::new("/tmp/input.rs");
-
-    let mut input = String::new();
-    let mut f = std::fs::File::open(fpath).unwrap();
-    f.read_to_string(&mut input);
-    let input = config::Input::Str(input);
-    // let input = config::Input::Str("
-    // struct FooStruct;
-    // fn fn2() -> FooStruct {}
-
-    // fn myfn(apple: &FooStruct){ 
-    //      let barge = fn2(); 
-    // }".to_string());
+    let fpath = Path::new(&fname);
+    let input = config::Input::File(fpath.to_path_buf());
 
     options.debugging_opts.verbose = true;
     options.unstable_features = config::UnstableFeatures::Default;
@@ -106,7 +97,10 @@ fn main() {
     let sess = session::build_session_(options, Some(fpath.to_path_buf()), span_diagnostic_handler);
     let krate_config = Vec::new();
 
+    // parse a the crate
     let krate = driver::phase_1_parse_input(&sess, krate_config, &input);
+
+    
     let krate = driver::phase_2_configure_and_expand(&sess, krate, "a", None)
                     .expect("phase 2 aborted");
 
@@ -142,11 +136,40 @@ fn main() {
                           lang_items,
                           stability::Index::new(krate));
     
-    rustc_typeck::check_crate(&tcx, trait_map);
+    // The following is from typeck::check_crate(&ty_cx, trait_map);
+
+
+    let ccx = CrateCtxt {
+        trait_map: trait_map,
+        all_traits: RefCell::new(None),
+        tcx: &tcx
+    };
+
+    collect::collect_item_types(&tcx);
 
     let t2 = time::precise_time_s();
 
-    println!("time is {} {}", (t1-t0), (t2-t0));
+    variance::infer_variance(&tcx);
+
+    let t3 = time::precise_time_s();
+
+    // Can we do without coherence checking?
+
+    // this is from     coherence::check_coherence(&ccx);
+    coherence::CoherenceChecker {
+        crate_context: &ccx,
+        inference_context: new_infer_ctxt(ccx.tcx),
+        inherent_impls: RefCell::new(FnvHashMap()),
+    }.check(&ccx.tcx.map.krate());
+
+
+    let t4 = time::precise_time_s();
+
+    check::check_item_types(&ccx);
+
+    let t5 = time::precise_time_s();
+
+    println!("time is {} {} {} {} {}", (t1-t0), (t2-t0), (t3-t0), (t4-t0), (t5-t0));
     println!("PHIL node_types is {:?}\n",tcx.node_types());
     println!("PHIL def_map is {:?}\n",tcx.def_map);
     println!("PHIL krate is {:?}",krate);
